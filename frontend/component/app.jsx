0bc516ce89ad196660486c7eb8976d7eba7d265d
Jian <jxw7410@rit.edu>
2019-05-30 16:21:45 -0400

    preparing to work on Channel

--- frontend/component/app.jsx
+++ frontend/component/app.jsx
@@ -4,12 +4,12 @@ import { AuthRoute } from '../util/auth_route'
 import SignUpContainer  from '../component/auth/signup_container'
 import LoginContainer from '../component/auth/login_container'
 import SplashComponent from '../component/splash/splash';
-import Search from '../component/search/search'
+import ChannelContainer from '../component/channel/channel_container';
 
 const App = () => (
     <div>
         <Route  exact path='/' component={SplashComponent}/>
-        <Route path='/search' component={Search} />
+        <Route path='/channel/:channel_id' component={ChannelContainer} />
         <AuthRoute path="/signup" component={SignUpContainer}/>  
         <AuthRoute path="/login" component={LoginContainer}/>  
     </div>
