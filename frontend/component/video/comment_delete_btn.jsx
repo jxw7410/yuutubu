883125cf28f1ea6dffa06b69c6a8a1e7291a2aad
Jian <jxw7410@rit.edu>
2019-11-14 17:08:15 -0500

    fix another glitch

--- frontend/component/video/comment_delete_btn.jsx
+++ frontend/component/video/comment_delete_btn.jsx
@@ -4,14 +4,14 @@ import { withRouter } from 'react-router-dom';
 import { requestDeletePost } from '../../actions/video_post/video_posts_action'
 
 const CommentDeleteBtn = props => {
-  const dropdownRef = React.useRef();
-  const [openDropdown, setOpenDropdown] = React.useState(false);
+  // const dropdownRef = React.useRef();
+  // const [openDropdown, setOpenDropdown] = React.useState(false);
   
-  React.useEffect(()=>{
-    if (openDropdown){
-      dropdownRef.current.focus();
-    } 
-  }, [openDropdown]);
+  // React.useEffect(()=>{
+  //   if (openDropdown){
+  //     dropdownRef.current.focus();
+  //   } 
+  // }, [openDropdown]);
 
   function handleDelete(postId) {
     return e => props.deletePost(postId)
@@ -22,28 +22,34 @@ const CommentDeleteBtn = props => {
     <>
       {
         props.isOwner ?
-          <i 
-            style={{
-              position: 'relative',
-              width: '30px',
-              height: '30px',
-              background: 'black'
-            }}
-            onClick={(e) => setOpenDropdown(!openDropdown)}
-            className='fas fa-ellipsis-v'>
-            <ul
-              ref={dropdownRef}
-              className='post-delete-dropdown'
-              tabIndex='0' 
-              onBlur={() => setOpenDropdown(false)}
-              style={openDropdown ? null : { display: 'none' }}>
-              <li
-                className='post-delete-dropdown-item flexh-1'
-                onClick={handleDelete(props.post.id)}>
-                Delete
-            </li>
-            </ul>
-          </i> : null
+          // <i 
+          //   style={{
+          //     position: 'relative',
+          //     width: '30px',
+          //     height: '30px',
+          //     background: 'black'
+          //   }}
+          //   onClick={(e) => setOpenDropdown(!openDropdown)}
+          //   className='fas fa-ellipsis-v'>
+          //   <ul
+          //     ref={dropdownRef}
+          //     className='post-delete-dropdown'
+          //     tabIndex='0' 
+          //     onBlur={() => setOpenDropdown(false)}
+          //     style={openDropdown ? null : { display: 'none' }}>
+          //     <li
+          //       className='post-delete-dropdown-item flexh-1'
+          //       onClick={handleDelete(props.post.id)}>
+          //       Delete
+          //   </li>
+          //   </ul>
+          // </i> : null
+
+          <button
+            className="form-del-btn"
+            onClick={handleDelete(props.post.id)}>
+            Delete
+          </button> : null
       }
     </>
   )
