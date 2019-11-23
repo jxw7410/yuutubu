0596840b081f5a3f80a3b1e9dfa148fe131d6a72
Jian <jxw7410@rit.edu>
2019-11-23 18:27:37 -0500

    modify delete button to include pop up

--- frontend/component/video/comment_delete_btn.jsx
+++ frontend/component/video/comment_delete_btn.jsx
@@ -2,58 +2,99 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { requestDeletePost } from '../../actions/video_post/video_posts_action'
+import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import { FlexHorizontalCentered } from '../../css-styles/css_styles';
 
 const CommentDeleteBtn = props => {
-  // const dropdownRef = React.useRef();
-  // const [openDropdown, setOpenDropdown] = React.useState(false);
-  
-  // React.useEffect(()=>{
-  //   if (openDropdown){
-  //     dropdownRef.current.focus();
-  //   } 
-  // }, [openDropdown]);
+  const [openDropdown, setOpenDropdown] = React.useState(false);
 
   function handleDelete(postId) {
-    return e => props.deletePost(postId)
+    return e => {
+      e.stopPropagation();
+      props.deletePost(postId)
+    }
   }
 
+  function handleOpenDropdown(bool) {
+    return e => {
+      e.currentTarget.focus();
+      setOpenDropdown(bool);
+    }
+  }
 
   return (
     <>
       {
         props.isOwner ?
-          // <i 
-          //   style={{
-          //     position: 'relative',
-          //     width: '30px',
-          //     height: '30px',
-          //     background: 'black'
-          //   }}
-          //   onClick={(e) => setOpenDropdown(!openDropdown)}
-          //   className='fas fa-ellipsis-v'>
-          //   <ul
-          //     ref={dropdownRef}
-          //     className='post-delete-dropdown'
-          //     tabIndex='0' 
-          //     onBlur={() => setOpenDropdown(false)}
-          //     style={openDropdown ? null : { display: 'none' }}>
-          //     <li
-          //       className='post-delete-dropdown-item flexh-1'
-          //       onClick={handleDelete(props.post.id)}>
-          //       Delete
-          //   </li>
-          //   </ul>
-          // </i> : null
-
-          <button
-            className="form-del-btn"
-            onClick={handleDelete(props.post.id)}>
-            Delete
-          </button> : null
+          <ThemeProvider theme={{ background: 'white' }}>
+            <IconButton
+              onClick={handleOpenDropdown(!openDropdown)}
+              onBlur={() => setOpenDropdown(false)}
+            >
+              <DropDown
+                onClick={e => e.stopPropagation()}
+                style={openDropdown ? null : { display: 'none' }}>
+                <DropDownItem
+                  onClick={handleDelete(props.post.id)}>
+                  Delete
+              </DropDownItem>
+              </DropDown>
+            </IconButton>
+          </ThemeProvider>
+          : null
       }
     </>
   )
 }
+
+
+const EllipsisV = props => (
+  <i className={`fas fa-ellipsis-v ${props.className}`}
+    tabIndex='0'
+    onBlur={props.onBlur}
+    onClick={props.onClick}>
+    {props.children}
+  </i>
+)
+
+const IconButton = styled(EllipsisV)`
+  position: relative;
+  text-align: center;
+  width: 30px;
+  background: ${props => props.theme.background || 'white'};
+
+  &:focus{
+    outline: none;
+  }
+
+  &:hover{
+    color: gray;
+  }
+`;
+
+const DropDown = styled.ul`
+  position: absolute;
+  z-index: 1000;
+  border: 1px solid gray;
+  border-radius: 3px;
+  width: 80px;
+  padding: 5px 0px;
+  background: inherit;
+`;
+
+const DropDownItem = styled.li`
+  ${FlexHorizontalCentered}
+  width: 100%;
+  height: 20px;
+  font-size: 14px;
+  color: black;
+
+  &:hover{
+    background: lightgray;
+  }
+`;
+
 
 const msp = (state, props) => ({
   isOwner: state.session.id == props.post.user_id
---
+++ frontend/css-styles/css_styles.jsx
@@ -0,0 +1,9 @@
+import {css} from 'styled-components';
+
+
+export const FlexHorizontalCentered = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
